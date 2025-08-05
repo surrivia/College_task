2640d16086306f80c7029c736afc0acf80cf633d
surrivia <bijoyhossain360@gmail.com>
2025-08-05 22:41:36 +0600

    dead

--- python/circumference_are_circle.py
+++ python/circumference_are_circle.py
@@ -1,9 +1,9 @@
 # A programme that take input of redius of a circle and print its circumference and area useing math.pi 
-import math
+import math as m
 redius = float(input("Enter Redius - "))
 
-circumference = 2 * math.pi * redius
-Area = math.pi * pow(redius,2)
+circumference = 2 * m.pi * redius
+Area = m.pi * redius**2
 
 print("circumference : ",circumference)
 print("radius : ", Area)
